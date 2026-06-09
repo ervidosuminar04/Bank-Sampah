605887cb551c4beee563453f25b97843269aa608
Ervido Suminarhadi <ervidosuminar04@gmail.com>
2026-06-09 12:57:38 +0700

    feat: initialize database configuration and register application seeders

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,6 +14,7 @@ class DatabaseSeeder extends Seeder
         $this->call([
             AdminSeeder::class,
             NasabahSeeder::class,
+            PengepulSeeder::class,
             MasterSeeder::class,
         ]);
     }
